c13c35a7110e4a484bf789d40508007fb0fcb81f
Nemanja Pokrajac <nemanja.pokrajac@bay42.io>
2016-02-01 19:17:27 +0100

    Added dynamic load page on index

---
+++ app/js/dynamicLoad.js
@@ -0,0 +1,49 @@
+(function () {
+    // region Prototypes
+    NodeList.prototype.forEach = Array.prototype.forEach;
+    //endregion
+
+
+    function createCORSRequest(method, url) {
+        method = method || 'GET';
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            xhr.open(method, url, true);
+        } else if (typeof XDomainRequest != "undefined") {
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        } else {
+            throw new Error('CROS not supported');
+        }
+        return xhr;
+    }
+
+    function loadLink(link) {
+        var xhr = createCORSRequest('GET', link);
+        xhr.onload = function () {
+            document.querySelector('main').innerHTML = xhr.responseText;
+        };
+        xhr.onerror = function () {
+            alert(xhr.status + ': ' + xhr.statusText);
+        };
+        xhr.send();
+    }
+
+    function removeActive(link) {
+        var currnt_link = link;
+        document.querySelectorAll('nav a').forEach(function (item) {
+            if (item.href != currnt_link) {
+                item.classList.remove('active');
+            }
+        });
+    }
+
+    document.querySelectorAll('nav a').forEach(function (item) {
+        item.addEventListener("click", function (event) {
+            event.preventDefault();
+            item.classList.add('active');
+            removeActive(item.href);
+            loadLink(item.href);
+        }, false);
+    });
+})();
