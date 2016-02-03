469ad77a52f2fb03b2007fea977871465f20d93a
BorisHTML <borishtml5@gmail.com>
2016-02-03 01:59:56 +0100

    Jednostavni Java Script

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,12 @@
 (function(){
-	var prikaziOdgovoreBtn = document.getElementById('show-answers');
-	var pitanje = document.getElementById('q1');
-	var odgovori = pitanje.getElementsByTagName('li');
 
+	function showAnswer(){		
+
+		var btnId = this.id;
+		var btnNumber = btnId.replace( /^\D+/g, '');
 	
-	
-	
-	prikaziOdgovoreBtn.addEventListener("click", function(){
+		var pitanje = document.getElementById('q'+btnNumber);
+		var odgovori = pitanje.getElementsByTagName('li');
 		var idTacnogOdgovora = pitanje.getAttribute('data-correct-answer-id'); 
 		
 		for (var i = 0; i < odgovori.length; i++) {	
@@ -22,462 +22,70 @@
 				tacan = false;
 				odgovori[i].style.color = "red";											
 			}			
-		}		
-	});
+		}	
+		
+	}
+
+	var prikaziOdgovoreBtn1 = document.getElementById('show-answers1');
+	prikaziOdgovoreBtn1.addEventListener("click", showAnswer);
+
 
 	var prikaziOdgovoreBtn2 = document.getElementById('show-answers2');
-	
-	prikaziOdgovoreBtn2.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q2');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn2.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn3 = document.getElementById('show-answers3');
-
-	prikaziOdgovoreBtn3.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q3');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn3.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn4 = document.getElementById('show-answers4');
-
-	prikaziOdgovoreBtn4.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q4');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn4.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn5 = document.getElementById('show-answers5');
-
-	prikaziOdgovoreBtn5.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q5');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn5.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn6 = document.getElementById('show-answers6');
-
-	prikaziOdgovoreBtn6.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q6');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn6.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn7 = document.getElementById('show-answers7');
-
-	prikaziOdgovoreBtn7.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q7');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn7.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn8 = document.getElementById('show-answers8');
-
-	prikaziOdgovoreBtn8.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q8');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn8.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn9 = document.getElementById('show-answers9');
-
-	prikaziOdgovoreBtn9.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q9');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn9.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn10 = document.getElementById('show-answers10');
-
-	prikaziOdgovoreBtn10.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q10');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn10.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn11 = document.getElementById('show-answers11');
-
-	prikaziOdgovoreBtn11.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q11');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn11.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn12 = document.getElementById('show-answers12');
-
-	prikaziOdgovoreBtn12.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q12');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn12.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn13 = document.getElementById('show-answers13');
-
-	prikaziOdgovoreBtn13.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q13');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn13.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn14 = document.getElementById('show-answers14');
-
-	prikaziOdgovoreBtn14.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q14');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn14.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn15 = document.getElementById('show-answers15');
-
-	prikaziOdgovoreBtn15.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q15');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn15.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn16 = document.getElementById('show-answers16');
-
-	prikaziOdgovoreBtn16.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q16');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn16.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn17 = document.getElementById('show-answers17');
-
-	prikaziOdgovoreBtn17.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q17');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn17.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn18 = document.getElementById('show-answers18');
-
-	prikaziOdgovoreBtn18.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q18');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn18.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn19 = document.getElementById('show-answers19');
-
-	prikaziOdgovoreBtn19.addEventListener("click", function(){
-
-		var pitanjeV2 = document.getElementById('q19');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
+	prikaziOdgovoreBtn19.addEventListener("click", showAnswer);
 
 	var prikaziOdgovoreBtn20 = document.getElementById('show-answers20');
+	prikaziOdgovoreBtn20.addEventListener("click", showAnswer);
 
-	prikaziOdgovoreBtn20.addEventListener("click", function(){
 
-		var pitanjeV2 = document.getElementById('q20');
-		var odgovoriV2 = pitanjeV2.getElementsByTagName('li');
-		var idTacnogOdgovora = pitanjeV2.getAttribute('data-correct-answer-id'); 
-		
-		for (var i = 0; i < odgovoriV2.length; i++) {	
-			
-			var odgovor = odgovoriV2[i];
-			var isChecked = odgovor.getElementsByTagName('input')[0].checked;
-			
-			
-			if (odgovor.id === idTacnogOdgovora) {
-				odgovoriV2[i].style.color = "green";
-			} 
-			if (odgovor.id !== idTacnogOdgovora && isChecked){				
-				tacan = false;
-				odgovoriV2[i].style.color = "red";											
-			}			
-		}		
-	});
 }())
